0e403baa0841dc660771a83cd7953ebea05a639f
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:03:11 +0300

    Create respect fragment

---
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/respect/RespectFragment.kt
@@ -0,0 +1,19 @@
+package com.dogactanriverdi.humanity.presentation.respect
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dogactanriverdi.humanity.R
+import com.dogactanriverdi.humanity.common.viewBinding
+import com.dogactanriverdi.humanity.databinding.FragmentRespectBinding
+
+class RespectFragment : Fragment(R.layout.fragment_respect) {
+
+    private val binding by viewBinding(FragmentRespectBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
