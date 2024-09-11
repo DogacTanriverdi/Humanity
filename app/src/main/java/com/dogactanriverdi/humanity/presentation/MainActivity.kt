04ebf90b4d0a9c6f61dcd05f603e6f31112d7256
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:10:40 +0300

    Add FragmentContainerView to MainActivity and Setup navigation

--- app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dogactanriverdi.humanity.R
+import com.dogactanriverdi.humanity.common.viewBinding
+import com.dogactanriverdi.humanity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
