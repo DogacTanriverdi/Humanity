e50680f53e7a964138f0719fc02f876f0a069edf
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:01:41 +0300

    Create optimism fragment

---
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/optimism/OptimismFragment.kt
@@ -0,0 +1,19 @@
+package com.dogactanriverdi.humanity.presentation.optimism
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dogactanriverdi.humanity.R
+import com.dogactanriverdi.humanity.common.viewBinding
+import com.dogactanriverdi.humanity.databinding.FragmentOptimismBinding
+
+class OptimismFragment : Fragment(R.layout.fragment_optimism) {
+
+    private val binding by viewBinding(FragmentOptimismBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
