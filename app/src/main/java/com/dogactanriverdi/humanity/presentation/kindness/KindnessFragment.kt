00611acdb2c923b0dbdbcbb8f0f03fb437da452a
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 10:59:54 +0300

    Create kindness fragment

---
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/kindness/KindnessFragment.kt
@@ -0,0 +1,19 @@
+package com.dogactanriverdi.humanity.presentation.kindness
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dogactanriverdi.humanity.R
+import com.dogactanriverdi.humanity.common.viewBinding
+import com.dogactanriverdi.humanity.databinding.FragmentKindnessBinding
+
+class KindnessFragment : Fragment(R.layout.fragment_kindness) {
+
+    private val binding by viewBinding(FragmentKindnessBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
