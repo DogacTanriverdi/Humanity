5e051af6a03d15f8b6faffaab70d89177d94d52d
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:33:41 +0300

    Add text, button and ImageView to RespectFragment and add gif inside

--- app/src/main/java/com/dogactanriverdi/humanity/presentation/respect/RespectFragment.kt
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/respect/RespectFragment.kt
@@ -3,6 +3,7 @@ package com.dogactanriverdi.humanity.presentation.respect
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.dogactanriverdi.humanity.R
 import com.dogactanriverdi.humanity.common.viewBinding
 import com.dogactanriverdi.humanity.databinding.FragmentRespectBinding
@@ -13,7 +14,18 @@ class RespectFragment : Fragment(R.layout.fragment_respect) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
 
+            Glide.with(requireContext())
+                .asGif()
+                .load(R.drawable.respect)
+                .into(ivRespect)
 
+            btnF.setOnClickListener {
+                tvPressFToPayRespects.visibility = View.GONE
+                btnF.visibility = View.GONE
+                ivRespect.visibility = View.VISIBLE
+            }
+        }
     }
 }
