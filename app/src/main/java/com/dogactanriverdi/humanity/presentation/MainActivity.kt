0b14d10ae69abdc1b46e9b033af7342cc20c6a65
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:52:05 +0300

    Check switch states and set bottom nav menu

--- app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.dogactanriverdi.humanity.presentation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.size
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import com.dogactanriverdi.humanity.R
 import com.dogactanriverdi.humanity.common.viewBinding
 import com.dogactanriverdi.humanity.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    private val viewModel: SharedViewModel by viewModels()
+
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +62,109 @@ class MainActivity : AppCompatActivity() {
             navController.addOnDestinationChangedListener { _, destination, _ ->
                 bottomNavigationView.menu.findItem(destination.id)?.isChecked = true
             }
+
+            bottomNavView = bottomNavigationView
+
+            bottomNavView.menu.add(
+                Menu.NONE,
+                R.id.homeFragment,
+                Menu.NONE,
+                "Home"
+            ).setIcon(R.drawable.ic_home)
+
+            with(viewModel) {
+
+                lifecycleScope.launch {
+                    egoSwitch.collect { egoSwitchState ->
+                        if (egoSwitchState.isChecked) {
+                            bottomNavigationView.visibility = View.GONE
+                        } else {
+                            bottomNavigationView.visibility = View.VISIBLE
+                        }
+                    }
+                }
+
+                lifecycleScope.launch {
+                    otherSwitchesStates.collect { otherSwitchesStates ->
+                        updateBottomNavigation(otherSwitchesStates.isOtherSwitchesChecked)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateBottomNavigation(switchStates: List<Boolean>) {
+        val menu = bottomNavView.menu
+
+        updateMenuItem(
+            menu,
+            R.id.givingFragment,
+            switchStates[0],
+            "Giving",
+            R.drawable.ic_favorite
+        )
+
+        updateMenuItem(
+            menu,
+            R.id.happinessFragment,
+            switchStates[1],
+            "Happiness",
+            R.drawable.ic_happy_face
+        )
+        updateMenuItem(
+            menu,
+            R.id.kindnessFragment,
+            switchStates[2],
+            "Kindness",
+            R.drawable.ic_flower
+        )
+        updateMenuItem(
+            menu,
+            R.id.optimismFragment,
+            switchStates[3],
+            "Optimism",
+            R.drawable.ic_brightness
+        )
+        updateMenuItem(
+            menu,
+            R.id.respectFragment,
+            switchStates[4],
+            "Respect",
+            R.drawable.ic_handshake
+        )
+    }
+
+    private fun updateMenuItem(
+        menu: Menu,
+        itemId: Int,
+        isChecked: Boolean,
+        title: String,
+        iconId: Int
+    ) {
+        val menuItem = menu.findItem(itemId)
+        if (isChecked) {
+            if (menuItem == null) {
+                if (menu.size < 5) {
+                    menu.add(Menu.NONE, itemId, Menu.NONE, title).setIcon(iconId)
+                } else {
+                    viewModel.toggleOtherSwitches(getSwitchIndexById(itemId), false)
+                }
+            }
+        } else {
+            menuItem?.let {
+                menu.removeItem(itemId)
+            }
+        }
+    }
+
+    private fun getSwitchIndexById(itemId: Int): Int {
+        return when (itemId) {
+            R.id.givingFragment -> 0
+            R.id.happinessFragment -> 1
+            R.id.kindnessFragment -> 2
+            R.id.optimismFragment -> 3
+            R.id.respectFragment -> 4
+            else -> -1
         }
     }
 }
--- app/src/main/java/com/dogactanriverdi/humanity/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/home/HomeFragment.kt
@@ -1,19 +1,128 @@
 package com.dogactanriverdi.humanity.presentation.home
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.CycleInterpolator
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.dogactanriverdi.humanity.R
 import com.dogactanriverdi.humanity.common.viewBinding
 import com.dogactanriverdi.humanity.databinding.FragmentHomeBinding
+import com.dogactanriverdi.humanity.presentation.EgoSwitchState
+import com.dogactanriverdi.humanity.presentation.OtherSwitchesState
+import com.dogactanriverdi.humanity.presentation.SharedViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val binding by viewBinding(FragmentHomeBinding::bind)
 
+    private val viewModel: SharedViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            with(viewModel) {
 
+                switchEgo.setOnCheckedChangeListener { _, isChecked ->
+                    toggleEgoSwitch(isChecked)
+                }
+
+                switchGiving.setOnCheckedChangeListener { _, isChecked ->
+                    if (switchEgo.isChecked) {
+                        shakeScreen()
+                        switchGiving.isChecked = false
+                    } else {
+                        toggleOtherSwitches(0, isChecked)
+                    }
+                }
+
+                switchHappiness.setOnCheckedChangeListener { _, isChecked ->
+                    if (switchEgo.isChecked) {
+                        shakeScreen()
+                        switchHappiness.isChecked = false
+                    } else {
+                        toggleOtherSwitches(1, isChecked)
+                    }
+                }
+
+                switchKindness.setOnCheckedChangeListener { _, isChecked ->
+                    if (switchEgo.isChecked) {
+                        shakeScreen()
+                        switchKindness.isChecked = false
+                    } else {
+                        toggleOtherSwitches(2, isChecked)
+                    }
+                }
+
+                switchOptimism.setOnCheckedChangeListener { _, isChecked ->
+                    if (switchEgo.isChecked) {
+                        shakeScreen()
+                        switchOptimism.isChecked = false
+                    } else {
+                        toggleOtherSwitches(3, isChecked)
+                    }
+                }
+
+                switchRespect.setOnCheckedChangeListener { _, isChecked ->
+                    if (switchEgo.isChecked) {
+                        shakeScreen()
+                        switchRespect.isChecked = false
+                    } else {
+                        toggleOtherSwitches(4, isChecked)
+                    }
+                }
+
+                observeEgoSwitchState(egoSwitch)
+                observeOtherSwitchesState(otherSwitchesStates)
+            }
+        }
+    }
+
+    private fun shakeScreen() {
+        val animator = ObjectAnimator.ofFloat(
+            binding.root,
+            "translationX",
+            0f,
+            25f,
+            -25f,
+            25f,
+            -25f,
+            15f,
+            -15f,
+            6f,
+            -6f,
+            0f
+        )
+        animator.duration = 500
+        animator.interpolator = CycleInterpolator(0.3f)
+        animator.start()
+    }
+
+    private fun observeEgoSwitchState(state: StateFlow<EgoSwitchState>) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            state.collect { egoSwitchState ->
+                binding.switchEgo.isChecked = egoSwitchState.isChecked
+            }
+        }
+    }
+
+    private fun observeOtherSwitchesState(state: StateFlow<OtherSwitchesState>) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            state.collect { otherSwitchesState ->
+                with(binding) {
+                    val isOtherSwitchesChecked = otherSwitchesState.isOtherSwitchesChecked
+                    switchGiving.isChecked = isOtherSwitchesChecked[0]
+                    switchHappiness.isChecked = isOtherSwitchesChecked[1]
+                    switchKindness.isChecked = isOtherSwitchesChecked[2]
+                    switchOptimism.isChecked = isOtherSwitchesChecked[3]
+                    switchRespect.isChecked = isOtherSwitchesChecked[4]
+                }
+            }
+        }
     }
 }
