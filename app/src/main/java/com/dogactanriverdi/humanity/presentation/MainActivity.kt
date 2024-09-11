a6012c91d0a4397b7e98c1602a174accf98f71c5
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-09-11 11:19:14 +0300

    Setup bottom nav bar and add manuel bottom bar navigation

--- app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
+++ app/src/main/java/com/dogactanriverdi/humanity/presentation/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import com.dogactanriverdi.humanity.R
 import com.dogactanriverdi.humanity.common.viewBinding
 import com.dogactanriverdi.humanity.databinding.ActivityMainBinding
@@ -21,6 +24,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val navOptions = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setPopUpTo(
+                navController.graph.findStartDestination().id,
+                false,
+                saveState = true
+            )
+            .setRestoreState(true)
+            .build()
+
+        with(binding) {
+
+            bottomNavigationView.setOnItemSelectedListener {
+                navController.navigate(it.itemId, null, navOptions)
+                true
+            }
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                bottomNavigationView.menu.findItem(destination.id)?.isChecked = true
+            }
         }
     }
 }
